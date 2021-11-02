0fabd1edd609389abf2b961dca21d585b042b636
YusunL <47683668+YusunL@users.noreply.github.com>
2021-11-02 23:23:24 +0900

    add category, comment, nested comment models

--- notice_board/apps/posts/models.py
+++ notice_board/apps/posts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from apps.users.models import User
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
 class Post(models.Model):
     """Custom Post Model"""
 
@@ -10,3 +14,22 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
+    hit = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    """Custom Comment Model"""
+
+    post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+
+class NestedComment(models.Model):
+    """Custom Nested-comment Model"""
+
+    post = models.ForeignKey(Post, on_delete=models.DO_NOTHING)
+    comment = models.ForeignKey(Comment, on_delete=models.DO_NOTHING)
+    body = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
