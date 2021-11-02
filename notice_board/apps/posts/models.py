2943a438bdf5ed44e8f24d1f9aa94217cbb316df
YusunL <47683668+YusunL@users.noreply.github.com>
2021-11-02 22:13:55 +0900

    add posts model

--- notice_board/apps/posts/models.py
+++ notice_board/apps/posts/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from apps.users.models import User
 
 
 class Post(models.Model):
-    pass
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+    writer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
