80ee36101743c31fab20ff973c9f88f0a88a8512
YusunL <47683668+YusunL@users.noreply.github.com>
2021-11-02 22:38:04 +0900

    modify posts model(add variables and changed vaiable names)

--- notice_board/apps/posts/models.py
+++ notice_board/apps/posts/models.py
@@ -7,4 +7,6 @@ class Post(models.Model):
 
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=2000)
-    writer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
